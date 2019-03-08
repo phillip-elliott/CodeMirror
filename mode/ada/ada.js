ac96b844985e36cae072ea3470f9d32988f6ed7e
Phillip Elliott <philliptelliott@gmail.com>
2019-03-08 10:43:59 -0500

    Add indentation support

--- mode/ada/ada.js
+++ mode/ada/ada.js
@@ -15,7 +15,7 @@
     return new RegExp("^((" + words.join(")|(") + "))\\b");
   }
 
-  const keywords = wordRegexp([
+  var commonKeywords = [
     "abort", "abstract", "accept",
     "access", "aliased", "all",
     "array", "at", "begin", "body",
@@ -34,21 +34,29 @@
     "tagged", "task", "terminate", "then",
     "type", "until", "use", "when",
     "while", "with"
-  ])
+  ]
 
-  const wordOperators = wordRegexp(["and", "or", "xor", "not", "and then", "or else", "abs", "mod", "rem", "in", "not in"])
+  var commonWordOperators = ["and", "or", "xor", "not", "and then", "or else", "abs", "mod", "rem", "in", "not in"]
   
-  const isPunctuationChar = /[\[\]{}\(\),;\:\.]/
-  const isOperators = /^([-+*/&<>=])/
-  const numberStart = /[\d\.]/
-  const number = /^(?:0x[a-f\d]+|0b[01]+|(?:\d+\.?\d*|\.\d+)(?:e[-+]?\d+)?)(u|ll?|l|f)?/i
-  const identifiers = /^[A-Za-z][_A-Za-z0-9\u00A1-\uFFFF]*/
-  const isChar = /('([^']|[']{2})')/
+  var isPunctuationChar = /[\[\]{}\(\),;\:\.]/
+  var isOperators = /^([-+*/&<>=])/
+  var numberStart = /[\d\.]/
+  var number = /^(?:0x[a-f\d]+|0b[01]+|(?:\d+\.?\d*|\.\d+)(?:e[-+]?\d+)?)(u|ll?|l|f)?/i
+  var identifiers = /^[A-Za-z][_A-Za-z0-9\u00A1-\uFFFF]*/
+  var isChar = /('([^']|[']{2})')/
+
+  var indentWords = wordRegexp(["package", "procedure", "function", "if", "for", "declare"])
 
 
-  CodeMirror.registerHelper("hintWords", "ada", keywords);
+  CodeMirror.registerHelper("hintWords", "ada", commonKeywords.concat(commonWordOperators));
 
   CodeMirror.defineMode("ada", function(config, parserConfig) {
+
+    var keywords = wordRegexp(commonKeywords.concat(parserConfig.extraKeywords))
+    var wordOperators = wordRegexp(commonWordOperators)
+    var builtIns = wordRegexp(parserConfig.customBuiltins)
+    var identifierCache = {}
+
     function tokenBase(stream, state) {
       if (stream.eatSpace()) return null;
 
@@ -56,16 +64,22 @@
         return "keyword";
       }
 
+      if(stream.match(builtIns)){
+        return "builtin";
+      }
+
       if(stream.match(identifiers)) {
-        if (state.lastToken == "procedure" || state.lastToken == "body" || state.lastToken == "function" || state.lastToken == "type" || state.lastToken == "end")
-          return "def"
+        if (state.lastToken == "procedure" || state.lastToken == "body" || state.lastToken == "function" || state.lastToken == "type" || state.lastToken == "end") {
+          identifierCache[stream.current()] = stream.current;
+          return "def";
+        }
       }
 
       if(stream.match(isChar)) {
-        return "string"
+        return "string";
       }
 
-      let ch = stream.next();
+      var ch = stream.next();
 
       // comments start with --
       if (ch == "-" && stream.eat("-")) {
@@ -74,13 +88,13 @@
       }
 
       if (ch == '"') {
-        return tokenAtString(stream, ch)
+        return tokenAtString(stream, ch);
       }
 
       if(numberStart.test(ch)) {
-        stream.backUp(1)
+        stream.backUp(1);
         if (stream.match(number)) return "number"
-        stream.next()
+        stream.next();
       }
       
       if (isPunctuationChar.test(ch)) {
@@ -99,7 +113,7 @@
     }
 
     function tokenAtString(stream, check) {
-      let next;
+      var next;
       while ((next = stream.next()) != null) {
         if (next == check && !stream.eat(check)) {
           break;
@@ -112,21 +126,51 @@
       startState: function () {
         return {
           tokenize: tokenBase,
-          lastToken: null
+          lastToken: null,
+          lastWord: null,
+          dedenting: true,
+          indented: 0,
+          context: {type: "top", indented: -config.indentUnit}
         }
       },
       token: function(stream, state) {
-        if (stream.sol()) stream.indentation();
-        let style = state.tokenize(stream, state);
+        if (stream.sol()) state.indented = stream.indentation();
+        var style = state.tokenize(stream, state);
+        var word = stream.current();
+        var kwtype;
+
         if (style) {
-          state.lastToken = style == "keyword" ? stream.current() : null
+          state.lastToken = style == "keyword" ? word : null;
         }
-        return style
+
+        if (word == "end") {
+          kwtype = "dedent";
+        } else if (style == "keyword" && indentWords.test(word)) {
+          if(word == "if" && state.lastWord == "end"){
+            kwtype = "";
+          } else
+            kwtype = "indent";
+        } 
+        
+        if (kwtype == "indent") {
+          state.context = {prev: state.context, type: style, indented: state.indented}
+        } else if (kwtype == "dedent") {
+          state.context = state.context.prev 
+        }
+        
+        state.lastWord = style == "keyword" ? word : state.lastWord;
+
+        return style;
       },
-      indent: null,
+      indent: function(state, textAfter) {
+        var ct = state.context;
+        var closing = ct.type == "keyword" &&  (/^(?:end|else|elsif|begin|is|then)\b/.test(textAfter));
+        var value = ct.indented + (closing ? 0 : config.indentUnit);
+        return value;
+      },
       lineComment: "--",
       fold: "indent",
-      electricInput: null
+      electricInput: /^\s*(?:end|else|elsif|begin|is|then)\b/
     }
   })
 
